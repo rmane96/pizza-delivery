c77c7883dd163b126b7a333c6440ccdbaef5d632
rmane96 <rmane9641@gmail.com>
2022-09-05 16:44:11 +0530

    created model

--- pizza/settings.py
+++ pizza/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'orders.apps.OrdersConfig',
+    # third party
+    'rest_framework',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'authentication.User'
