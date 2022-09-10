73907f3272c6020c9199cdfa9a5818c44f6ca2b2
rmane96 <rmane9641@gmail.com>
2022-09-10 20:34:01 +0530

    heroku config

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
-web gunicorn pizza.wsgi --log-file
+release: python manage.py migrate --no-input
+web gunicorn pizza.wsgi --log-file -
--- pizza/settings.py
+++ pizza/settings.py
@@ -124,10 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 STATIC_ROOT = BASE_DIR/ 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 
